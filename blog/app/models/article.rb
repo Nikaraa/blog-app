6830081b7ab61434eeea2382de023aee93590a91
nikaraa <giuseppemalara00001@gmail.com>
2025-04-19 13:49:45 +0200

    Add Delete Comments operations and created Visible module for Articles and comments status

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
-    has_many :comments #1:n association
-
+    include Visible
+  
+    has_many :comments, dependent :destroy # on delete cascade
+  
     validates :title, presence: true
-    validates :body, presence: true, length: {minimum: 10}
-end
+    validates :body, presence: true, length: { minimum: 10 }
+  end
+  
